09391cb5e0a18a9313c157f8846e7d46aab6007e
vignerd <omerbcalik@gmail.com>
2021-08-09 00:27:30 +0300

    favicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" href="http://omerberkancalik.com/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap"
             rel="stylesheet"
